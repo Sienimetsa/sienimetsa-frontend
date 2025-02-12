7a886b1c3192c83030dc9f6a935ece74b57629ee
Maajoo <mio.kantola@hotmail.com>
2025-02-12 13:44:18 +0200

    add SettingScreen and navigation to it from HomeScreen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './Screens/LoginScreen';
 import SignupScreen from './Screens/SignupScreen';
 import HomeScreen from './Screens/HomeScreen';
+import SettingScreen from './Screens/SettingScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';  
 import { makeProtectedRequest } from '../Service/AuthService';  
 
 const HomeScreen = () => {
   const [data, setData] = useState(null);
+  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +20,10 @@ const HomeScreen = () => {
     <View>
       <Text>Protected Data:</Text>
       {data ? <Text>{JSON.stringify(data)}</Text> : <Text>Home</Text>}
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
     </View>
   );
 };
